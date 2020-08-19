f982ba3e9b778497293e13924f418ae6cb3b00cc
ReiKohaku <hbsjzjwj@163.com>
2020-08-19 18:56:20 +0800

    Add LICENSE

--- src/lib/api/statistics.js
+++ src/lib/api/statistics.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 
 const Statistics = {
   new: async (type, id) => {
+    return;
     try {
       let data = await Vue.prototype.$axios.post(`https://api.bangplayer.reikohaku.fun/play/statistics/new`, {
         type,
@@ -13,11 +14,13 @@ const Statistics = {
     }
   },
   latest: async  () => {
+    return;
     let data = await Vue.prototype.$axios.post(`https://api.bangplayer.reikohaku.fun/play/statistics/latest`, {size: 10});
     if (!data.status) throw new Error(data.error);
     return data;
   },
   hot: async () => {
+    return;
     let data = await Vue.prototype.$axios.post(`https://api.bangplayer.reikohaku.fun/play/statistics/hot`, {size: 10});
     if (!data.status) throw new Error(data.error);
     return data;
