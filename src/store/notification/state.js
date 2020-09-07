b57b1af503438493930e6fb066778a396e4d8277
ReiKohaku <hbsjzjwj@163.com>
2020-09-07 13:58:59 +0800

    Fix bug of mark as read of notification

--- src/store/notification/state.js
+++ src/store/notification/state.js
@@ -1,7 +1,6 @@
 import {LocalStorage} from "quasar";
 
 export default function () {
-  LocalStorage.remove('notification');
   return {
     notification: [],
     readList: LocalStorage.getItem('notification') || []
